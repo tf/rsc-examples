48e01f13c0e59c5b60ff2b0d00f196b3254c6552
Tim Fischbach <tfischbach@codevise.de>
2025-01-09 22:13:24 +0100

    Try to add lazy component to client component
    Error when starting the server:
    
        SyntaxError: Cannot use 'import.meta' outside a module
            at internalCompileFunction (node:internal/vm:73:18)
            at wrapSafe (node:internal/modules/cjs/loader:1176:20)
            at Module._compile (node:internal/modules/cjs/loader:1218:27)
            at Module._extensions..js (node:internal/modules/cjs/loader:1308:10)
            at Module.load (node:internal/modules/cjs/loader:1117:32)
            at Module._load (node:internal/modules/cjs/loader:958:12)
            at Module.require (node:internal/modules/cjs/loader:1141:19)
            at require (node:internal/modules/cjs/helpers:110:18)
            at Object.<anonymous> (/home/tfischbach/code/parcel-rsc-examples/examples/server/dist/Todos.a1afc280.
        js:1:65)
        at Module._compile (node:internal/modules/cjs/loader:1254:14)

--- examples/server/src/Dialog.tsx
+++ examples/server/src/Dialog.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { ReactNode, useRef } from "react";
+import { ReactNode, useRef, lazy, Suspense } from "react";
 
-export function Dialog({trigger, children}: {trigger: ReactNode, children: ReactNode}) {
+const ExpensiveThing = lazy(() => import("./ExpensiveThing"));
+
+export function Dialog({ trigger, children }: { trigger: ReactNode, children: ReactNode }) {
   let ref = useRef<HTMLDialogElement | null>(null);
   return (
     <>
       <button onClick={() => ref.current?.showModal()}>{trigger}</button>
       <dialog ref={ref} onSubmit={() => ref.current?.close()}>
+        <Suspense fallback={<div>Loading</div>}>
+          <ExpensiveThing />
+        </Suspense>
+
+
         {children}
       </dialog>
     </>
